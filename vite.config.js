bf619cd7cf9f05ff5e4f107bb1072007b424e586
Nick Timperon <nicholas.timperon@gmail.com>
2025-02-28 09:36:52 +1100

    Fix build configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/figure-grid/'
+  base: '/figure-grid/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
